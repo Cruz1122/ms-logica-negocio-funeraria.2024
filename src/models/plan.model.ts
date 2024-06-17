daf75a91f97328972e072f148ce0722c617237cb
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2024-06-16 20:22:34 -0500

    Adición de propiedad 'foto' en el modelo del plan

--- src/models/plan.model.ts
+++ src/models/plan.model.ts
@@ -1,8 +1,8 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {ClientePlan} from './cliente-plan.model';
-import {Servicio} from './servicio.model';
 import {PlanxServicio} from './planx-servicio.model';
 import {ServicioFunerario} from './servicio-funerario.model';
+import {Servicio} from './servicio.model';
 
 @model()
 export class Plan extends Entity {
@@ -36,10 +36,22 @@ export class Plan extends Entity {
   })
   precio: number;
 
+  @property({
+    type: 'string',
+    required: true,
+  })
+  foto: string;
+
   @hasMany(() => ClientePlan, {keyTo: 'idPlan'})
   clientesPlanes: ClientePlan[];
 
-  @hasMany(() => Servicio, {through: {model: () => PlanxServicio, keyFrom: 'idPlan', keyTo: 'idServicio'}})
+  @hasMany(() => Servicio, {
+    through: {
+      model: () => PlanxServicio,
+      keyFrom: 'idPlan',
+      keyTo: 'idServicio',
+    },
+  })
   servicios: Servicio[];
 
   @hasMany(() => ServicioFunerario, {keyTo: 'idPlan'})
